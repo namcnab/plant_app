43242d30ccf7dac87baa32986f981d52a24a16df
namcnab <nnekaxmcnab@gmail.com>
2024-06-22 12:11:48 -0700

    Adds cards containing plant info to the homepage.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,55 @@
-import { Container, Box } from "@mui/material";
+import {
+  Container,
+  Box,
+  Grid,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+
+import data from "config/data.json";
 
 export default function Home() {
   return (
     <>
       <Container maxWidth="xl">
-        <Box sx={{ height: "75vh", m: 2 }}>
-          <img
-            src={require("../../src/assets/images/home_page_header.png")}
-            alt="placeholder image"
-          />
-        </Box>
+        <Grid container>
+          <Grid item xs={12}>
+            <Box sx={{ m: 2 }}>
+              <img
+                src={require("../../src/assets/images/home/home_page_header.png")}
+                alt="placeholder image"
+              />
+            </Box>
+          </Grid>
+        </Grid>
+        <Grid container justifyContent="center">
+          {data.data.map((item) => (
+            <Grid item xs={3} sx={{ m: 2 }}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  sx={{ height: 250 }}
+                  image={require("assets/images/plants/" + item.img)}
+                  title="green monstera plant"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {item.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {item.description}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </Container>
     </>
   );
