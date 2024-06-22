80b4a1c0954076bdfe725376de37837d6cd03f68
namcnab <nnekaxmcnab@gmail.com>
2024-06-22 13:12:17 -0700

    Creates a separate component to handle the rendering of plant cards.

---
+++ src/components/PlantCard.tsx
@@ -0,0 +1,66 @@
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  Grid,
+} from "@mui/material";
+
+import { useNavigate } from "react-router-dom";
+
+interface PlantData {
+  data: {
+    img: string;
+    title: string;
+    name: string;
+    description: string;
+  }[];
+}
+
+interface PlantCardProps {
+  plants: PlantData;
+}
+
+function PlantCard(props: PlantCardProps) {
+  const navigate = useNavigate();
+
+  const navigateToPlantDetails = (id: string) => {
+    navigate("/plant_details", { state: { id } });
+  };
+
+  return (
+    <>
+      {props.plants.data.map((item: any) => (
+        <Grid item xs={3} sx={{ m: 2 }}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              sx={{ height: 250 }}
+              image={require("assets/images/plants/" + item.img)}
+              title={item.title}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {item.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {item.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button
+                size="small"
+                onClick={() => navigateToPlantDetails(item.id)}
+              >
+                Learn More
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </>
+  );
+}
+
+export default PlantCard;
