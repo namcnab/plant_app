35e552508bce5921082458876782e8d545aaa58e
namcnab <nnekaxmcnab@gmail.com>
2024-06-23 13:25:54 -0700

    Adds additional plant information.

--- src/pages/PlantDetails.tsx
+++ src/pages/PlantDetails.tsx
@@ -1,19 +1,69 @@
 import { useLocation } from "react-router-dom";
 import data from "config/data.json";
+import { Box, Container, Grid } from "@mui/material";
 
 export default function PlantDetails() {
   const location = useLocation();
   const plant = data.data.find((item) => item.id === location.state.id);
 
   return (
-    <div>
-      <h2>{plant.name}</h2>
-      <img
-        src={require("assets/images/plants/" + plant.img)}
-        alt={plant.title}
-        style={{ maxHeight: "75vh" }}
-      />
-      <h3>Average Height: {plant.height}</h3>
-    </div>
+    <Container maxWidth="xl">
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={12}>
+          <h1>{plant.common_name}</h1>
+        </Grid>
+        <Grid item xs={12}>
+          <img
+            src={require("assets/images/plants/" + plant.additionalImg)}
+            alt={plant.imgAlt}
+            width={250}
+            height={250}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Box sx={{ m: 2 }}>
+            <p>
+              <b>Botanical Name:</b> {plant.scientific_name}
+            </p>
+            <p>
+              <b>Origin:</b> {plant.origin}
+            </p>
+            <p>
+              <b>Description:</b> {plant.description}
+            </p>
+            <p>
+              <b>History:</b> {plant.history}
+            </p>
+            <h2>Plant Care</h2>
+            <p>
+              <b>Light:</b> {plant.care.light}
+            </p>
+            <p>
+              <b>Water:</b> {plant.care.water}
+            </p>
+            <p>
+              <b>Temperature:</b> {plant.care.temperature}
+            </p>
+            <p>
+              <b>Humidity:</b> {plant.care.humidity}
+            </p>
+            <p>
+              <b>Soil:</b> {plant.care.soil}
+            </p>
+            <p>
+              <b>Fertilizer:</b> {plant.care.fertilizer}
+            </p>
+            <p>
+              <b>Propagation:</b> {plant.care.propogation}
+            </p>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
