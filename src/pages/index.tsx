95185361c77345a262423f83b89d090e83bf24d2
namcnab <nnekaxmcnab@gmail.com>
2024-06-23 16:27:05 -0700

    Removes the Plant Care component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Home from "./Home";
 import Glossary from "./Glossary";
-import PlantCare from "./PlantCare";
 import PlantDetails from "./PlantDetails";
-export { Home, Glossary, PlantCare, PlantDetails };
+export { Home, Glossary, PlantDetails };
