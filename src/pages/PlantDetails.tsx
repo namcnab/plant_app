84ffa6fed21fc892111edd8b1fdeac70fc6d5075
namcnab <nnekaxmcnab@gmail.com>
2024-06-23 13:59:08 -0700

    Puts plant care information into accordions.

--- src/pages/PlantDetails.tsx
+++ src/pages/PlantDetails.tsx
@@ -1,10 +1,40 @@
 import { useLocation } from "react-router-dom";
 import data from "config/data.json";
-import { Box, Container, Grid } from "@mui/material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Container,
+  Divider,
+  Grid,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import WaterDropIcon from "@mui/icons-material/WaterDrop";
+import ThermostatIcon from "@mui/icons-material/Thermostat";
+import OpacityIcon from "@mui/icons-material/Opacity";
+import PublicIcon from "@mui/icons-material/Public";
+import GrassIcon from "@mui/icons-material/Grass";
+import ContentCutIcon from "@mui/icons-material/ContentCut";
+
+const iconMapping = {
+  light: <WbSunnyIcon sx={{ mr: 1 }} />,
+  water: <WaterDropIcon sx={{ mr: 1 }} />,
+  temperature: <ThermostatIcon sx={{ mr: 1 }} />,
+  humidity: <OpacityIcon sx={{ mr: 1 }} />,
+  soil: <PublicIcon sx={{ mr: 1 }} />,
+  fertilizer: <GrassIcon sx={{ mr: 1 }} />,
+  propogation: <ContentCutIcon sx={{ mr: 1 }} />,
+};
 
 export default function PlantDetails() {
   const location = useLocation();
   const plant = data.data.find((item) => item.id === location.state.id);
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 
   return (
     <Container maxWidth="xl">
@@ -39,28 +69,25 @@ export default function PlantDetails() {
             <p>
               <b>History:</b> {plant.history}
             </p>
+
+            <Divider></Divider>
+
             <h2>Plant Care</h2>
-            <p>
-              <b>Light:</b> {plant.care.light}
-            </p>
-            <p>
-              <b>Water:</b> {plant.care.water}
-            </p>
-            <p>
-              <b>Temperature:</b> {plant.care.temperature}
-            </p>
-            <p>
-              <b>Humidity:</b> {plant.care.humidity}
-            </p>
-            <p>
-              <b>Soil:</b> {plant.care.soil}
-            </p>
-            <p>
-              <b>Fertilizer:</b> {plant.care.fertilizer}
-            </p>
-            <p>
-              <b>Propagation:</b> {plant.care.propogation}
-            </p>
+            {Object.entries(plant.care).map(([key, value]) => (
+              <Accordion key={key}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1-content"
+                  id="panel1-header"
+                >
+                  <Box display="flex" alignItems="center">
+                    {iconMapping[key] || <WbSunnyIcon sx={{ mr: 1 }} />}
+                    <h2>{capitalizeFirstLetter(key)}</h2>
+                  </Box>
+                </AccordionSummary>
+                <AccordionDetails>{value}</AccordionDetails>
+              </Accordion>
+            ))}
           </Box>
         </Grid>
       </Grid>
